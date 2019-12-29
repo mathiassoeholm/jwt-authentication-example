24f071c64b5c02784d72228b446cc97fb41ff94b
Mathias Søholm <mss@trifork.com>
2019-12-29 11:49:08 +0100

    Remove unwanted stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { UnauthenticatedApp } from "./UnauthenticatedApp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <UnauthenticatedApp />;
 }
 
 export default App;
---
+++ src/UnauthenticatedApp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UnauthenticatedApp() {
+  return <div>login</div>;
+}
+
+export { UnauthenticatedApp };
