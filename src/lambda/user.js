fccb86fdb638ab55b328c6dd75c0147fc290fbfe
Mathias Søholm <mss@trifork.com>
2020-01-11 11:32:39 +0100

    Stringify response

--- src/lambda/user.js
+++ src/lambda/user.js
@@ -4,10 +4,8 @@ import jwt from "jsonwebtoken";
 
 export async function handler(event) {
   const cookies = event.headers.cookie && cookie.parse(event.headers.cookie);
-  console.log("user 1");
 
   if (!cookies || !cookies.jwt) {
-    console.log("user 2", cookies);
     return {
       statusCode: 401,
       body: JSON.stringify({
@@ -15,20 +13,17 @@ export async function handler(event) {
       })
     };
   }
-  console.log("user 3");
 
   try {
     const payload = jwt.verify(cookies.jwt, publicKey);
-    console.log("user 4", payload);
     return {
       statusCode: 200,
       headers: {
         "Content-Type": "application/json"
       },
-      body: payload
+      body: JSON.stringify({ userId: payload.userId, email: payload.email })
     };
   } catch (err) {
-    console.log("user 5", err);
     return {
       statusCode: 401,
       body: JSON.stringify({ msg: err.message })
